784cfcd8821b2140eb5e038588f836cb278c12ac
V-Paritosh <paritoshv08@gmail.com>
2024-10-14 00:01:11 -0500

    added portfolio to the navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,13 +3,13 @@ import './index.scss'
 import Logo from '../../assets/images/logo.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      <img className="p-logo"src={Logo} alt="logo" />
+      <img className="p-logo" src={Logo} alt="logo" />
       <img classname="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
     <nav>
@@ -23,6 +23,14 @@ const Sidebar = () => (
         to="/about"
       >
         <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="portfolio-link"
+        to="/portfolio"
+      >
+        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4d" />
       </NavLink>
       <NavLink
         exact="true"
