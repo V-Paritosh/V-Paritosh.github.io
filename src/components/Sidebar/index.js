d303fa94ba5b083ddf3446d4e28814c4bcae1316
V-Paritosh <paritoshv08@gmail.com>
2024-10-15 23:40:02 -0500

    made it all mobile friendly

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,65 +3,101 @@ import './index.scss'
 import Logo from '../../assets/images/logo.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+  faBars,
+  faClose
+} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img className="p-logo" src={Logo} alt="logo" />
-      <img classname="sub-logo" src={LogoSubtitle} alt="slobodan" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4d" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4d" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4d" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/V-Paritosh"
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img className="p-logo" src={Logo} alt="logo" />
+        <img classname="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          to="/"
         >
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4d" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/paritosh-vaghasiya-61921a2b8/"
+          <FontAwesomeIcon icon={faHome} color="#4d4d4d" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4d" />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+          <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4d" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4d" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/V-Paritosh"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4d" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/paritosh-vaghasiya-61921a2b8/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4d" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
 
 export default Sidebar
