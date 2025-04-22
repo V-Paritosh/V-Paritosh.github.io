2d4afc6635b0615b6797ad7b17569e0f4a79bcdf
V-Paritosh <paritoshv08@gmail.com>
2025-04-21 22:53:05 -0500

    dark bg

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,6 @@ const navLinks = [
   { name: "Experience", href: "#experience" },
   { name: "Projects", href: "#projects" },
   { name: "Skills", href: "#skills" },
-  { name: "Contact", href: "#contact" },
 ];
 
 export default function Navbar() {
@@ -59,9 +58,9 @@ export default function Navbar() {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300${
-        isScrolled ? "bg-black/85 backdrop-blur-md py-3" : "bg-transparent py-5"
-      }`} // Keep bg-black on mobile and transparent on larger screens
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-black${
+        isScrolled ? " py-3 " : " py-5 "
+      }`}
     >
       <div className="container mx-auto px-4 md:px-6">
         <div className="relative flex items-center justify-between">
